ee20e7853a42bfe0df386f41b7b446efc0bb64c2
Ivana M <maksimovic.protonmail.com>
2024-01-04 13:57:27 +0100

    improved responsiveness and added i18n

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,13 +4,16 @@ import myImage from '../../assets/me.gif'
 import {GiGraduateCap} from 'react-icons/gi'
 import {BsBookmarkStar} from 'react-icons/bs'
 import {TfiWorld} from 'react-icons/tfi'
+import { BsDribbble } from "react-icons/bs"
+import { useTranslation } from 'react-i18next';
 
 const About = () => {
+  const { t } = useTranslation();
   return (
     <section id='about'>
       
-      <h5>Get To Know</h5>
-      <h2>About Me</h2>
+      <h5>{t('get to know')}</h5>
+      <h2>{t('About Me')}</h2> {/* About Me */}
 
       <div className="container about__container">
 
@@ -25,49 +28,63 @@ const About = () => {
 
               <article className='about__card'>
                   <GiGraduateCap className='about__icon'/>
-                  <h5>Degree</h5>
-                  <small>Computer Science Expert <br />Software Development<br /><i>IHK Munich </i></small>
+                  <h5>{t('degree')} </h5>
+                  <small>{t('Computer Science Expert')} <br />{t('Software Development')}<br /><i>IHK Munich </i></small>
               </article>
 
               <article className='about__card'>
                   <BsBookmarkStar className='about__icon'/>
-                  <h5>Talk to me in:</h5>
+                  <h5>{t('Talk to me in')}:</h5>
                   <small>
                     <ul>
-                      <li>English</li>
-                      <li>German</li>
-                      <li>Spanish</li>
-                      <li>Serbian</li>
-                      <li>Polish</li>
-                      <li>Portuguese</li>
+                      <li>{t('English')}</li>
+                      <li>{t('German')}</li>
+                      <li>{t('Spanish')}</li>
+                      <li>{t('Serbian')}</li>
+                      <li>{t('Polish')}</li>
+                      <li>{t('Portuguese')}</li>
                     </ul>
                   </small>
               </article>
 
               <article className='about__card'>
                   <TfiWorld className='about__icon'/>
-                  <h5>Experience</h5>
+                  <h5>{t('Experience')}:</h5>
                   <small>
                     <ul>
-                      <li>Internship Year</li>
-                      <li>@Novatec GmbH</li>
-                      <li>Fulltime</li>
-                      <li>2022-2023</li>
-                      <li>Full Stack Development</li>
+                      <li>{t('Internship Year')}</li>
+                      <li>{t('@Novatec GmbH')}</li>
+                      <li>{t('Fulltime')}</li>
+                      <li>{t('2022-2023')}</li>
+                      <li>{t('Full Stack Development')}</li>
+                    </ul>
+                  </small>
+              </article>
+
+              <article className='about__card'>
+                  <BsDribbble className='about__icon'/>
+                  <h5>{t('Hobbies:')}</h5>
+                  <small>
+                    <ul>
+                      <li>{t('Basketball')}</li>
+                      <li>{t('IT-Trends')}</li>
+                      <li>{t('Traveling')}</li>
+                      <li>{t('Psychology')}</li>
+                      <li>{t('Gardening')}</li>
                     </ul>
                   </small>
               </article>
 
             </div>
             <p>
-              As an enthusiastic <b>Junior Software Developer</b>, 
-              I possess a strong passion for problem-solving and 
-              creating innovative solutions. 
-              I thrive in collaborative environments, actively sharing ideas and embracing teamwork.
-              With a grateful attitude, I approach challenges with a positive mindset, always eager to learn and grow.
-              Outside of the tech world, I enjoy exploring different cultures, languages, 
-              and engaging in recreational activities such as playing basketball.
-              I am committed to making a meaningful contribution to the industry and constantly expanding my skillset.
+              {t('As an enthusiastic')}<b>{t('Junior Software Developer')}</b>, 
+              {t('I possess a strong passion for problem-solving and')}
+              {t('creating innovative solutions.')} 
+              {t('I thrive in collaborative environments, actively sharing ideas and embracing teamwork.')}
+              {t('With a grateful attitude, I approach challenges with a positive mindset, always eager to learn and grow.')}
+              {t('Outside of the tech world, I enjoy exploring different cultures, languages')}, 
+              {t('and engaging in recreational activities such as playing basketball')}.
+              {t('I am committed to making a meaningful contribution to the industry and constantly expanding my skillset.')}
             </p>
 
           </div>
---
+++ src/i18n.jsx
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import enTranslation from './locales/en.json';
+import deTranslation from './locales/de.json';
+
+const resources = {
+  en: {
+    translation: enTranslation
+  },
+  de: {
+    translation: deTranslation
+  }
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en',
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
